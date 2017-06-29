eae0a75f7015d0d52c3df9cd04b85b378d877c38
rakusan <github@kuramochi.info>
2017-06-29 13:07:22 +0900

    CoreImageを追加するPostProcessBuild

---
+++ Assets/Editor/PostProcessBuild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	[PostProcessBuild]
+	public static void OnPostProcessBuild(BuildTarget buildTarget, string path) {
+		if (buildTarget == BuildTarget.iOS) {
+			string projPath = PBXProject.GetPBXProjectPath (path);
+
+			PBXProject proj = new PBXProject ();
+			proj.ReadFromString (File.ReadAllText (projPath));
+
+			string target = proj.TargetGuidByName ("Unity-iPhone");
+			proj.AddFrameworkToProject (target, "CoreImage.framework", false);
+
+			File.WriteAllText(projPath, proj.WriteToString());
+		}
+	}
+	
+}
